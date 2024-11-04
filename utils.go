3bc64ed74237e6ded0af6107baed7176e3c143d7
BillyHar12 <185959531+BillyHar12@users.noreply.github.com>
2024-11-05 00:00:00 +0500

    'add file utils.go'

--- utils.go
+++ utils.go
@@ -1,19 +1,3 @@
 package main
 
-import (
-	"os"
-	"os/exec"
-	"runtime"
-)
-
-// ClearConsole clears the console screen.
-func ClearConsole() {
-	var cmd *exec.Cmd
-	if runtime.GOOS == "windows" {
-		cmd = exec.Command("cmd", "/c", "cls")
-	} else {
-		cmd = exec.Command("clear")
-	}
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-}
+// Currently empty, but you can add utility functions here in the future.
